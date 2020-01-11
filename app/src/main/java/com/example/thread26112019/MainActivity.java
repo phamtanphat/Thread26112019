65d8d35755bb6858700be763bf69cf71fe60dcbd
phatdroid94 <phatdroid94@gmail.com>
2020-01-11 18:24:53 +0700

    thread state

--- app/src/main/java/com/example/thread26112019/MainActivity.java
+++ app/src/main/java/com/example/thread26112019/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.thread26112019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,17 +15,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // 1 : Khoi tao ra thread
-//        Thread thread = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//        thread.start();
+
 //        TaskThread taskThread = new TaskThread();
 //        taskThread.run();
-        Thread thread = new Thread(new RunableWork());
+//        Thread thread = new Thread(new RunableWork());
+//        thread.start();
+        final Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("BBB","Runnable runing");
+            }
+        });
         thread.start();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("BBB",thread.getState().toString());
+            }
+        },1000);
     }
 
 }
